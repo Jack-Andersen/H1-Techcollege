de9a6d74882b78a4dc3ba0da539de51a4b8705cc
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2020-10-22 08:26:46 +0200

    Trying to Commit

---
+++ H1-Projects/New Calculator/New Calculator/Class's/Calculator methods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace New_Calculator.Class_s
+{
+    public static class Calculator_methods
+    {
+        public static int Add(int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1 + number2;
+            return answer;
+        }
+
+        public static int Substraction(int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1 - number2;
+            return answer;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1 / number2;
+            return answer;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            int number1 = x;
+            int number2 = y;
+            int answer = number1 * number2;
+            return answer;
+        }
+
+
+
+    }
+}
