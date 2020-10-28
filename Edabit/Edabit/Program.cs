3ed7dabdad2b5971c8ac16495f10475922b0435c
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2020-10-28 11:16:23 +0100

    Worked on Edabit

--- Edabit/Edabit/Program.cs
+++ Edabit/Edabit/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 using System.Xml.Schema;
 
@@ -321,7 +322,7 @@ namespace Edabit
 
 
 
-            //Return a String as an Integer
+            //Return a String as an Integer 14
 
             //Create a function that takes a string and returns it as an integer.
 
@@ -342,7 +343,7 @@ namespace Edabit
 
 
 
-            //Find the Perimeter of a Rectangle
+            //Find the Perimeter of a Rectangle 15
 
             //Create a function that takes length and width and finds the perimeter of a rectangle.
 
@@ -365,7 +366,7 @@ namespace Edabit
 
 
 
-            //Reverse an Array
+            //Reverse an Array 16
 
             //Write a function to reverse an array.
 
@@ -389,7 +390,7 @@ namespace Edabit
 
 
 
-            //Add, Subtract, Multiply or Divide ?
+            //Add, Subtract, Multiply or Divide ? 17
 
             //Write a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided to get 24.If none of the operations can give 24, return "none".
 
@@ -430,7 +431,7 @@ namespace Edabit
 
 
 
-            //Is the String Odd or Even?
+            //Is the String Odd or Even? 18
 
             //Given a string, return true if its length is even or false if the length is odd.
 
@@ -445,8 +446,301 @@ namespace Edabit
 
             static string OddOrEven(string word)
             {
-                
+
+                    return (word.Length % 2 == 0).ToString();
+
             }
+
+
+
+            //Both Zero, Negative or Positive 19
+
+            //Write a function that returns true if both numbers are:
+
+            //Smaller than 0, OR...
+            //Greater than 0, OR...
+            //Exactly 0
+            //Otherwise, return false.
+
+            //Examples
+            //Both(6, 2) ➞ true
+
+            //Both(0, 0) ➞ true
+
+            //Both(-1, 2) ➞ false
+
+            //Both(0, 2) ➞ false
+            //Notes
+            //Inputs will always be two numbers.
+
+            static bool Both(int n1, int n2)
+            {
+                if ((n1 + n2 == 0) || (n1 > 0 && n2 > 0) || n1 < 0 && n2 < 0)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+
+
+            //Recursion: Length of a String 20
+
+            //Write a function that returns the length of a string.Make your function recursive.
+
+            //Examples
+            //Length("apple") ➞ 5
+
+            //Length("make") ➞ 4
+
+            //Length("a") ➞ 1
+
+            //Length("") ➞ 0
+            //Notes
+            //Check the Resources tab for info on recursion.
+
+            static int Length(string str) => str.Length;
+
+
+
+            //Name Greeting! 21
+
+            //Create a function that takes a name and returns a greeting in the form of a string.
+
+            //Examples
+            //HelloName("Gerald") ➞ "Hello Gerald!"
+
+            //HelloName("Tiffany") ➞ "Hello Tiffany!"
+
+            //HelloName("Ed") ➞ "Hello Ed!"
+            //Notes
+            //The input is always a name(as string).
+            //Don't forget the exclamation mark!
+            //If you get stuck on a challenge, find help in the Resources tab.
+            //If you're really stuck, unlock solutions in the Solutions tab.
+
+            static string HelloName(string name)
+            {
+                return "Hello " + name + "!";
+            }
+
+
+
+            //Are the Numbers Equal? 22
+
+            //Create a function that takes two integers and checks if they are equal.
+
+            //Examples
+            //IsEqual(5, 6) ➞ false
+
+            //IsEqual(1, 1) ➞ true
+
+            //IsEqual(36, 35) ➞ false
+            //Notes
+            //N / A
+
+            static bool IsEqual(int num1, int num2)
+            {
+                if (num1.Equals(num2))
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+
+
+            //Char - to - ASCII 23
+
+            //Create a function that returns the ASCII value of the passed in character.
+
+            //Examples
+            //CharToASCII('S') ➞ 83
+
+            //CharToASCII('m') ➞ 109
+
+            //CharToASCII('1') ➞ 49
+
+            //CharToASCII('a') ➞ 97
+            //Notes
+            //Don't forget to return the result.
+            //If you get stuck on a challenge, find help in the Resources tab.
+            //If you're really stuck, unlock solutions in the Solutions tab.
+
+            static int CharToASCII(char ch)
+            {
+
+                int i = ch;
+                return i;
+
+            }
+
+
+
+            //Drinks Allowed? 24
+
+            //A bartender is writing a simple program to determine whether he should serve drinks to someone.He only serves drinks to people 18 and older and when he's not on break.
+
+            //Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
+
+            //Examples
+            //ShouldServeDrinks(17, true) ➞ false
+
+            //ShouldServeDrinks(19, false) ➞ true
+
+            //ShouldServeDrinks(30, true) ➞ false
+            //Notes
+            //Return true or false.
+            //Some countries have a slightly higher drinking age, but for the purposes of this challenge, it will be 18.
+
+            static bool ShouldServeDrinks(int age, bool on_break)
+            {
+                if (age >= 18 && !on_break)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+
+            
+            //Lowercase, Uppercase or Mixed ? 25
+
+            //Create a function which returns "upper" if all the letters in a word are uppercase, "lower" if lowercase and "mixed" for any mix of the two.
+
+            //Examples
+            //GetCase("whisper...") ➞ "lower"
+
+            //GetCase("SHOUT!") ➞ "upper"
+
+            //GetCase("Indoor Voice") ➞ "mixed"
+            //Notes
+            //Ignore punctuation, spaces and numbers.
+
+            static string GetCase(string str)
+            {
+
+                if (str == str.ToLower())
+                {
+                    return "lower";
+                }
+
+                else if (str == str.ToUpper())
+                {
+                    return "upper";
+                }
+
+                else
+                    return "mixed";
+            }
+
+
+            
+            //Concatenating First and Last Character of a String 26
+
+            //Creates a function that takes a string and returns the concatenated first and last character.
+
+            //Examples
+            //FirstLast("forza") ➞ "fa"
+
+            //FirstLast("kali") ➞ "ki"
+
+            //FirstLast("always") ➞ "as"
+
+            //FirstLast("love") ➞ "le"
+
+            //FirstLast("edabit") ➞ "et"
+            //Notes
+            //There is no empty string.
+
+            static string FirstLast(string str)
+            {
+                {
+                    string s = string.Empty;
+                    s += str[0];
+                    s += str[str.Length - 1];
+                    return s;
+                }
+            }
+
+
+
+            //On / Off Switches 27
+
+            //Create a function that returns how many possible outcomes can come from a certain number of switches(on / off). In other words, for a given number of switches, how many different combinations of on and off can we have ?
+
+            //Examples
+            //PosCom(1) ➞ 2
+
+
+            //PosCom(3) ➞ 8
+
+
+            //PosCom(10) ➞ 1024
+            //Notes
+            //All numbers will be whole and positive.
+
+            static int PosCom(int n)
+            {
+                double result = Math.Pow(2, n);
+
+                return (int)result;
+            }
+
+
+
+            //Smash Factor 28
+
+            //Smash factor is a term in golf that relates to the amount of energy transferred from the club head to the golf ball.The formula for calculating smash factor is ball speed divided by club speed.
+
+
+            //Create a function that takes ball speed bs and club speed cs as arguments and returns the smash factor to the nearest hundredth.
+
+
+            //Examples
+            //SmashFactor(139.4, 93.8) ➞ 1.49
+
+
+            //SmashFactor(181.2, 124.5) ➞ 1.46
+
+
+            //SmashFactor(154.7, 104.3) ➞ 1.48
+            //Notes
+            //Remember to round to the nearest hundredth.
+            //All values will be valid(so no dividing by zero).
+
+            static double SmashFactor(double a, double b)
+            {
+
+                return Math.Round(a / b, 2);
+
+            }
+
+
+
+            //Stack the Boxes 29
+
+            //Here's an image of four models. Some of the cubes are hidden behind other cubes. Model one consists of one cube. Model two consists of four cubes, and so on...
+
+            //Stack the Boxes
+
+            //Write a function that takes a number n and returns the number of stacked boxes in a model n levels high, visible and invisible.
+
+            //Examples
+            //StackBoxes(1) ➞ 1
+
+            //StackBoxes(2) ➞ 4
+
+            //StackBoxes(0) ➞ 0
+            //Notes
+            //Step n is a positive integer.
+
+            static int StackBoxes(int n);
+
+
 
         }
     }
