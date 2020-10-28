582a72a9ef51e49faa37a3a31e222332cc7b72b7
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2020-10-28 14:52:17 +0100

    Worked on Discord bot

---
+++ Skp-H1/Hygge_discord_bot/Hygge_discord_bot/Handlers/Dialogue/IDialogStep.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hygge_discord_bot.Handlers.Dialogue
+{
+    public interface IDialogueStep
+    {
+        Action<DiscordMessage> OnMessageAdded { get; set; }
+        IDialogueStep NextStep { get; }
+        Task<bool> ProcessStep(DiscordClient Client,
+                               DiscordChannel channel,
+                               DiscordUser user);
+    }
+}
