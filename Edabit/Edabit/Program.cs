1fdd0d682add6c82a3b9f3247f15c0b296c7f068
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2020-11-12 09:51:38 +0100

    Worked on Edabit

--- Edabit/Edabit/Program.cs
+++ Edabit/Edabit/Program.cs
@@ -2044,6 +2044,47 @@ namespace Edabit
                 double k = Math.Round(a * h * h + b * h + c, 2);
                 return new[] { h, k };
             }
+
+
+
+            //C* ns*r * d Str* ngs 76
+
+            //Someone has attempted to censor my strings by replacing every vowel with a *, l* k*th * s.Luckily, I've been able to find the vowels that were removed.
+
+            //Given a censored string and a string of the censored vowels, return the original uncensored string.
+
+            //Example
+            //uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo") ➞ "Where did my vowels go?"
+
+            //uncensor("abcd", "") ➞ "abcd"
+
+            //uncensor("*PP*RC*S*", "UEAE") ➞ "UPPERCASE"
+            //Notes
+            //The vowels are given in the correct order.
+            //The number of vowels will match the number of* characters in the censored string.
+
+            static string Uncensor(string str, string chrStr)
+            {
+                int alt = 0;
+
+                string res = null;
+
+                foreach (char chr in str)
+                {
+                    if (chr == '*')
+                    {
+                        res = res + chrStr[alt];
+
+                        alt++;
+                    }
+                    else res = res + chr;
+                }
+                return res;
+            }
+
+
+
+
         }
     }
 }
