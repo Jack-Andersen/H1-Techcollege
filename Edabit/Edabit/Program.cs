e9b95106ad3ed0b9bf5c9f367760a51204f2dc87
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2020-11-12 11:14:43 +0100

    Worked on Edabit

--- Edabit/Edabit/Program.cs
+++ Edabit/Edabit/Program.cs
@@ -2084,6 +2084,30 @@ namespace Edabit
 
 
 
+            //Word Endings 77
+
+            //Create a function that adds a string ending to each member in an array.
+
+            //Examples
+            //AddEnding(["clever", "meek", "hurried", "nice"], "ly")
+            //➞ ["cleverly", "meekly", "hurriedly", "nicely"]
+
+            //AddEnding(["new", "pander", "scoop"], "er")
+            //➞ ["newer", "panderer", "scooper"]
+
+            //AddEnding(["bend", "sharpen", "mean"], "ing")
+            //➞ ["bending", "sharpening", "meaning"]
+            //Notes
+            //Don't forget to return the result.
+            //If you get stuck on a challenge, find help in the Resources tab.
+            //If you're really stuck, unlock solutions in the Solutions tab.
+
+            static string[] AddEnding(string[] arr, string ending)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                    arr[i] += ending;
+                return arr;
+            }
 
         }
     }
