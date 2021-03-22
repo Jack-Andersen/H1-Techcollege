c23f686ca24d28863f6b8a6d091e3052e930a2e8
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2021-03-22 15:00:36 +0100

    New

---
+++ Skp-H2/Bank_account/Bank_account/Bank_account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bank_account
+{
+    public class Bank_account
+    {
+
+        public string Number { get; }
+        public string Owner { get; set; }
+        public double Balance { get; set; }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public Bank_account(string name, double balance)
+        {
+            this.Owner = name;
+            this.Balance = balance;
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public double Withdraw(double withdrawAmount)
+        {
+            if (Balance <= 0 || Balance - withdrawAmount < 0)
+            {
+                throw new ArgumentException("You don't have enough money");
+            }
+            else
+            {
+                Balance -= withdrawAmount;
+                return Balance;
+            }
+        }
+
+        public double Deposit(double depositAmount)
+        {
+            Balance += depositAmount;
+            return Balance;
+        }
+
+        public void Print()
+        {
+            Balance.ToString();
+        }
+
+        public double Getbalance { get => Balance; }
+    }
+}
---
+++ Skp-H2/Bank_account/Bank_account/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Bank_account
+{
+    public partial class Bank_account_form : Form
+    {
+
+        Bank_account account = new Bank_account("Jack", 3000);
+
+        public Bank_account_form()
+        {
+            InitializeComponent();
+
+            Balance_Click(this, null);
+
+        }
+
+        private void Balance_Click(object sender, EventArgs e)
+        {
+           double bankBalance = account.Getbalance;
+           Balance.Text = bankBalance.ToString();
+        }
+
+        private void Withdraw_Click(object sender, EventArgs e)
+        {
+            double withdrawAmount = double.Parse(WithdrawAmountAndDeposit.Text);
+            Balance.Text = account.Withdraw(withdrawAmount).ToString();
+        }
+
+        private void Deposit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WithdrawAmountAndDeposit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
